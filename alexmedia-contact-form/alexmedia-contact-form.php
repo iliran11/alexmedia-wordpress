bb10fa1af24ac776e2f49dc1de08611ffbb40c0b
Liran Cohen <iliran11@gmail.com>
2020-04-28 17:41:19 +0300

    stable wip for alexmedia form

--- alexmedia-contact-form/alexmedia-contact-form.php
+++ alexmedia-contact-form/alexmedia-contact-form.php
@@ -13,6 +13,9 @@ function initPlugin()
   define('ACF_ASSET_MANIFEST', ACF_WIDGET_PATH . '/build/asset-manifest.json');
   define('ACF4_INCLUDES', plugin_dir_path(__FILE__) . '/includes');
   write_log(ACF4_INCLUDES);
+
+  defined('ABSPATH') or die('Direct script access disallowed.');
+  require_once(ACF4_INCLUDES . '/enqueue.php');
 }
 
 add_action('init', 'initPlugin', 20);
