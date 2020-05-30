9479bc67268e619c16c8f7f59b6285577175bc32
Liran Cohen <iliran11@gmail.com>
2020-05-30 10:40:03 +0300

    header styling

--- alexmedia-selfer/functions.php
+++ alexmedia-selfer/functions.php
@@ -1,2 +1,3 @@
 <?php
 wp_enqueue_style('alexmedia-selfer-style', get_theme_file_uri('/assets/alexmedia-selfer.css'));
+wp_enqueue_style('alexmedia-selfer-header-style', get_theme_file_uri('/assets/alexmedia-header.css'));
