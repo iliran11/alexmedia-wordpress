d89e3b25af6430ac6d1f8b77e6a73e9ba25e9594
Liran Cohen <iliran11@gmail.com>
2020-06-06 16:10:31 +0300

    sass watch

--- alexmedia-selfer/functions.php
+++ alexmedia-selfer/functions.php
@@ -1,3 +1,4 @@
 <?php
 wp_enqueue_style('alexmedia-selfer-style', get_theme_file_uri('/dist/alexmedia-selfer.css'));
 wp_enqueue_style('alexmedia-selfer-header-style', get_theme_file_uri('/dist/alexmedia-header.css'));
+wp_enqueue_style('alexmedia-selfer-new-style', get_theme_file_uri('/dist/alexmedia-new-style'));
--- alexmedia-selfer/watch.sh
+++ alexmedia-selfer/watch.sh
@@ -1,3 +1,4 @@
 sass --watch \
   ./assets/header.scss:./dist/alexmedia-header.css \
-  ./assets/main.scss:./dist/alexmedia-selfer.css
+  ./assets/main.scss:./dist/alexmedia-selfer.css \
+  ./assets/new-styling.scss:./dist/alexmedia-new-style
