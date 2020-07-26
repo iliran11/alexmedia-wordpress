a508ec8db58762e28ec583fbcd72e5353646abef
Liran Cohen <iliran11@gmail.com>
2020-07-26 22:49:16 +0300

    wip

--- alexmedia-selfer/watch.sh
+++ alexmedia-selfer/watch.sh
@@ -1,4 +1,2 @@
 sass --watch \
-  ./assets/header.scss:./dist/alexmedia-header.css \
-  ./assets/main.scss:./dist/alexmedia-selfer.css \
-  ./assets/new-styling.scss:./dist/alexmedia-new-style.css
+  ./assets/alex-style.scss:./dist/alex-style.css \
